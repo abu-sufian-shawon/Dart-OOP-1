69c93dddbc27f8cae1200be7ada7c9021a1c37e0
Abu Sufian <you@example.com>
2021-02-06 11:58:46 +0600

    Initial Commit

---
+++ bin/OOP_Practice.dart
@@ -0,0 +1,14 @@
+void main(List<String> arguments) {
+  print('Hello world!');
+}
+
+
+class Car{
+  String _carName;
+  String _carCompany;
+  String _modelName;
+  String _engineSeries;
+  String _wheel;
+  String _color;
+
+}
